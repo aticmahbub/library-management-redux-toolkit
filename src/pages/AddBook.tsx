cffa8322afd9a4977c68051174d4d3821ac1e06a
aticcmahbub <aticcmahbub@gmail.com>
2025-08-17 17:44:20 +0600

    feat: add loading spinner and form validation on add book page

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -1,4 +1,4 @@
-import {useForm, type FieldValues, type SubmitHandler} from 'react-hook-form';
+import {useForm, type SubmitHandler, type FieldValues} from 'react-hook-form';
 import {
     Form,
     FormControl,
@@ -7,7 +7,6 @@ import {
     FormLabel,
     FormMessage,
 } from '@/components/ui/form';
-
 import {
     Select,
     SelectContent,
@@ -20,157 +19,224 @@ import {
 import {Input} from '@/components/ui/input';
 import {Button} from '@/components/ui/button';
 import {useCreateBookMutation} from '@/redux/api/baseApi';
+import toast, {Toaster} from 'react-hot-toast';
+import {Spinner} from '@/components/ui/shadcn-io/spinner';
+import {useNavigate} from 'react-router';
+import {Textarea} from '@/components/ui/textarea';
 
 export default function AddBook() {
-    const form = useForm();
+    const navigate = useNavigate();
 
-    const [createBook, data] = useCreateBookMutation();
+    const form = useForm({
+        defaultValues: {
+            title: '',
+            author: '',
+            genre: '',
+            isbn: '',
+            description: '',
+            copies: '',
+            available: true,
+        },
+    });
+
+    const [createBook, {isLoading}] = useCreateBookMutation();
+
     const onSubmit: SubmitHandler<FieldValues> = async (values) => {
-        createBook(values);
-        console.log({values, data});
+        try {
+            const result = await createBook(values).unwrap();
+            toast.success(result.message);
+            form.reset();
+
+            setTimeout(() => {
+                navigate('/books');
+            }, 1000);
+        } catch (error: any) {
+            console.error(error?.data?.error?.message);
+            toast.error(error?.data?.error?.message || 'Failed to create book');
+        }
     };
+
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
-                <FormField
-                    control={form.control}
-                    name='title'
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Title</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder='Enter book title'
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name='author'
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Author</FormLabel>
-                            <FormControl>
-                                <Input placeholder='Author name' {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name='genre'
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Genre</FormLabel>
-                            <FormControl>
-                                <Select
-                                    onValueChange={field.onChange}
-                                    value={field.value}
-                                >
-                                    <SelectTrigger className='w-[180px]'>
-                                        <SelectValue placeholder='Select a genre' />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectLabel>Genre</SelectLabel>
-                                            <SelectItem value='FICTION'>
-                                                Fiction
-                                            </SelectItem>
-                                            <SelectItem value='NON_FICTION'>
-                                                NON_FICTION
-                                            </SelectItem>
-                                            <SelectItem value='SCIENCE'>
-                                                Science
-                                            </SelectItem>
-                                            <SelectItem value='HISTORY'>
-                                                History
-                                            </SelectItem>
-                                            <SelectItem value='BIOGRAPHY'>
-                                                Biography
-                                            </SelectItem>
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select>
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+        <div>
+            <Toaster />
+            {isLoading ? (
+                <div className='flex justify-center py-10'>
+                    <Spinner variant='bars' size={64} />
+                </div>
+            ) : (
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='space-y-4'
+                    >
+                        {/* Title */}
+                        <FormField
+                            control={form.control}
+                            name='title'
+                            rules={{required: 'Title is required'}}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Title</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder='Enter book title'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
 
-                <FormField
-                    control={form.control}
-                    name='isbn'
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>ISBN</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type='number'
-                                    placeholder='Enter book ISBN'
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name='description'
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Description</FormLabel>
-                            <FormControl>
-                                <Input placeholder='Description' {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name='copies'
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Copies</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type='number'
-                                    placeholder='Copies'
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name='available'
-                    render={({field}) => (
-                        <FormItem className='flex items-center gap-2'>
-                            <FormControl>
-                                <input
-                                    type='checkbox'
-                                    checked={field.value}
-                                    onChange={(e) =>
-                                        field.onChange(e.target.checked)
-                                    }
-                                />
-                            </FormControl>
-                            <FormLabel>Available</FormLabel>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+                        {/* Author */}
+                        <FormField
+                            control={form.control}
+                            name='author'
+                            rules={{required: 'Author is required'}}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Author</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder='Author name'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
 
-                <Button type='submit'>Submit</Button>
-            </form>
-        </Form>
+                        {/* Genre */}
+                        <FormField
+                            control={form.control}
+                            name='genre'
+                            rules={{required: 'Genre is required'}}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Genre</FormLabel>
+                                    <FormControl>
+                                        <Select
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                        >
+                                            <SelectTrigger className='w-[180px]'>
+                                                <SelectValue placeholder='Select a genre' />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectGroup>
+                                                    <SelectLabel>
+                                                        Genre
+                                                    </SelectLabel>
+                                                    <SelectItem value='FICTION'>
+                                                        Fiction
+                                                    </SelectItem>
+                                                    <SelectItem value='NON_FICTION'>
+                                                        Non-Fiction
+                                                    </SelectItem>
+                                                    <SelectItem value='SCIENCE'>
+                                                        Science
+                                                    </SelectItem>
+                                                    <SelectItem value='HISTORY'>
+                                                        History
+                                                    </SelectItem>
+                                                    <SelectItem value='BIOGRAPHY'>
+                                                        Biography
+                                                    </SelectItem>
+                                                </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        {/* ISBN */}
+                        <FormField
+                            control={form.control}
+                            name='isbn'
+                            rules={{required: 'ISBN is required'}}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>ISBN</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type='number'
+                                            placeholder='Enter book ISBN'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        {/* Description */}
+                        <FormField
+                            control={form.control}
+                            name='description'
+                            rules={{required: 'Description is required'}}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Description</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            {...field}
+                                            placeholder='Type about the book.'
+                                            id='description'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        {/* Copies */}
+                        <FormField
+                            control={form.control}
+                            name='copies'
+                            rules={{required: 'Copies count is required'}}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Copies</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type='number'
+                                            placeholder='Copies'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        {/* Available */}
+                        <FormField
+                            control={form.control}
+                            name='available'
+                            render={({field}) => (
+                                <FormItem className='flex items-center gap-2'>
+                                    <FormControl>
+                                        <input
+                                            type='checkbox'
+                                            checked={field.value}
+                                            onChange={(e) =>
+                                                field.onChange(e.target.checked)
+                                            }
+                                        />
+                                    </FormControl>
+                                    <FormLabel>Available</FormLabel>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button type='submit'>Submit</Button>
+                    </form>
+                </Form>
+            )}
+        </div>
     );
 }
